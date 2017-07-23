5cd28592dc0d3b0d77f51079ce4b41a356fd2550
Manish Katyal <manish.katyal@gmail.com>
2017-07-23 13:01:56 -0700

    LeNet

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import numpy as np
 from scipy import misc
 
-from keras.layers import Input, Flatten, Dense, Lambda
+from keras.layers import Input, Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 from keras.models import Model, Sequential
 
 flags = tf.app.flags
@@ -46,17 +46,39 @@ def load_data(data_root):
         measurements.append(measurement)
     return np.array(images), np.array(measurements)
 
-X_train, y_train = load_data(data_root="data/data")
-print (X_train.shape)
-print (y_train.shape)
+def simple_ffn(data_root="data/data"):
+    X_train, y_train = load_data(data_root="data/data")
+    print (X_train.shape)
+    print (y_train.shape)
 
-print('Building model')
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Flatten())
-model.add(Dense(1))
+    print('Building model')
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Flatten())
+    model.add(Dense(1))
 
-model.compile(loss="mse", optimizer="adam")
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-print('Saving')
-model.save('model.h5')
+    model.compile(loss="mse", optimizer="adam")
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+    print('Saving')
+    model.save('model.h5')
+
+def lenet(data_root="data/data"):
+    X_train, y_train = load_data(data_root="data/data")
+    print('Building model')
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    
+    model.compile(loss="mse", optimizer="adam")
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+    print('Saving')
+    model.save('lenet_model.h5')
+
+lenet()    
