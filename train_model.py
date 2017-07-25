f99f2ff41bc1e0996ed08ae2e4fe2e569365838f
Manish Katyal <manish.katyal@gmail.com>
2017-07-25 06:25:47 -0700

    Data augmentation - flip steering

--- train_model.py
+++ train_model.py
@@ -12,12 +12,15 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
-flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-flags.DEFINE_integer('epochs', 50, "The number of epochs.")
-flags.DEFINE_integer('batch_size', 256, "The batch size.")
+flags.DEFINE_string('data_root', 'data/data', "Location of the unzipped data file.")
+flags.DEFINE_integer('epochs', 7, "The number of epochs.")
+flags.DEFINE_bool('augmentation_flip_images', True, "If True, generate additional training data by flipping images.")
+flags.DEFINE_integer('max_images', -1, "If positive, the maximum number of images to use. Used for quick iteration.")
 
-def read_dataset(data_root):
+def flip_images(img, steering):
+    return np.fliplr(img), -float(steering)    
+
+def read_dataset(data_root=FLAGS.data_root):
     read_image = lambda fname : misc.imread(os.path.join(data_root, fname.strip()))
     with open(os.path.join(data_root, "driving_log.csv")) as f:
         # 'center','left','right','steering','throttle','brake','speed'
@@ -32,22 +35,28 @@ def read_dataset(data_root):
                 print(i)
             if i % 100 == 0:
                 print("Processed:", i)
+            if FLAGS.max_images > 0 and i >= FLAGS.max_images:
+                return
             # TODO: left and right as well.
             # yield (left, center, right) (row['steering'], row['throttle'], row['brake'], row['speed'])
             yield center, row['steering']
             yield left, row['steering']
             yield right, row['steering']
+            if FLAGS.augmentation_flip_images:
+                yield flip_images(center, row['steering'])
+                yield flip_images(left, row['steering'])
+                yield flip_images(right, row['steering'])
 
-def load_data(data_root):
+def load_data():
     images = []
     measurements = []
-    for center_img, measurement in read_dataset(data_root):
+    for center_img, measurement in read_dataset():
         images.append(center_img)
         measurements.append(measurement)
     return np.array(images), np.array(measurements)
 
-def simple_ffn(data_root="data/data"):
-    X_train, y_train = load_data(data_root="data/data")
+def simple_ffn():
+    X_train, y_train = load_data()
     print (X_train.shape)
     print (y_train.shape)
 
@@ -58,12 +67,12 @@ def simple_ffn(data_root="data/data"):
     model.add(Dense(1))
 
     model.compile(loss="mse", optimizer="adam")
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
     print('Saving')
     model.save('model.h5')
 
-def lenet(data_root="data/data"):
-    X_train, y_train = load_data(data_root="data/data")
+def lenet():
+    X_train, y_train = load_data()
     print('Building model')
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -77,8 +86,9 @@ def lenet(data_root="data/data"):
     model.add(Dense(1))
     
     model.compile(loss="mse", optimizer="adam")
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-    print('Saving')
-    model.save('lenet_model.h5')
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
+    model_name = 'lenet_aug_model.h5' if FLAGS.augmentation_flip_images else 'lenet_model.h5'
+    print('Saving to:', model_name)
+    model.save(model_name)
 
 lenet()    
