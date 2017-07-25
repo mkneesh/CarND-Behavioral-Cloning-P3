7b4262c9002c950af8aea369f8f7c39e067d7224
Manish Katyal <manish.katyal@gmail.com>
2017-07-25 07:36:52 -0700

    NVidia network architecture

--- train_model.py
+++ train_model.py
@@ -105,4 +105,30 @@ def lenet():
     print('Saving to:', model_name)
     model.save(model_name)
 
-lenet()    
+def nvidia():
+    X_train, y_train = load_data()
+    print('Building model')
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    
+    model.compile(loss="mse", optimizer="adam")
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
+    model_name = 'nvidia.h5'
+    if FLAGS.augmentation_flip_images:
+        model_name = "aug_" + model_name
+    if FLAGS.augmentation_multiple_cameras:
+        model_name = "mc_" + model_name
+    print('Saving to:', model_name)
+    model.save(model_name)
+    
+nvidia()    
